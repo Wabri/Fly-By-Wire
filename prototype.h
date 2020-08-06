52d961ba4a6f4efc56291797b86bcc31ae84fde9
Wabri <gabriele.puliti@protonmail.com>
2020-08-06 17:59:04 +0200

    Add prototype: function declarations and implementation

---
+++ prototype.h
@@ -0,0 +1,21 @@
+
+// Dichiarazione funzioni
+// <tipo> <nome> ([<tipo-parametro>[,...]]);
+
+int test(const char *yolo, ...);
+
+long test2(int);
+
+void test3(void);
+
+void test4(); // dichiarazione della solita fuzione test3, ma incorretta secondo lo standard ANSI
+
+// Implementazione funzione
+// <tipo> <nome> ([<tipo> <parametro>[,...]]) {
+//  <istruzione>; ...
+// }
+
+int prodotto (int x, int y) {
+    return x*y;
+}
+
