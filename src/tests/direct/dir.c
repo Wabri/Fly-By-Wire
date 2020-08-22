3087b825efd4db33029d45fc73de4f2d281c6af6
Wabri <gabriele.puliti@protonmail.com>
2020-08-22 16:52:09 +0200

    Update dynamics and dir

--- src/tests/direct/dir.c
+++ src/tests/direct/dir.c
@@ -1,4 +1,5 @@
 #include "inc.h"
+#include <bits/types/__FILE.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,15 +9,32 @@ int main(int argc, char *argv[]) {
   printf("%d\n", THREE);
   printf("%d\n", DOUBLE(THREE));
 
-  int test = argv[1];
+  int test = 8;
 
 #if test == 0
-  printf("compilato 1<10");
+  printf("compilato 1<10\n");
 #elif test == 1
-  printf("compilato 10>1000");
+  printf("compilato 10>1000\n");
 #else
-  printf("compilato sempre");
+  printf("compilato sempre\n");
 #endif
+
+#ifdef TEST
+  printf("TEST importato con successo\n");
+#endif
+#ifndef TEST
+  printf("Non verrà stampato\n");
+#endif
+#undef TEST
+#ifndef TEST
+  printf("TEST eliminato con successo\n");
+#endif
+
+  printf("%s\n", __FILE__);
+  printf("%d\n", __LINE__);
+  printf("%s\n", __DATE__);
+  printf("%s\n", __TIME__);
+  printf("%d\n", __STDC__);
 
   exit(EXIT_SUCCESS);
 }
