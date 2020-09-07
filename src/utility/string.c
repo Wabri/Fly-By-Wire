8bb5149b264445055d8ade36e728497dc771f7ca
Wabri <gabriele.puliti@protonmail.com>
2020-09-07 09:27:50 +0200

    Add string: create method to extract a substring

--- src/utility/string.c
+++ src/utility/string.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int strExtrSeparator(char *dest, char *source, const char *separetor) {
@@ -6,3 +8,15 @@ int strExtrSeparator(char *dest, char *source, const char *separetor) {
   return 0;
 }
 
+int strExtrInterval(char *dest, char *source, int start, int end) {
+  if (start <= end) {
+    for (int i = start, j = 0; i <= end; i++, j++) {
+      if (source[i] != '\0') {
+        dest[j] = source[i];
+      }
+    }
+    return 0;
+  } else {
+    return 1;
+  }
+}
