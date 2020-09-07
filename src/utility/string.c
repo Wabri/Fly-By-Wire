c24f809af9ceeeba862a0e7c1db3e78f26933c0a
Wabri <gabriele.puliti@protonmail.com>
2020-09-07 19:58:53 +0200

    Update string: change type of extract interval function

--- src/utility/string.c
+++ src/utility/string.c
@@ -8,15 +8,15 @@ int strExtrSeparator(char *dest, char *source, const char *separetor) {
   return 0;
 }
 
-int strExtrInterval(char *dest, char *source, int start, int end) {
+void strExtrInterval(char *dest, char *source, int start, int end) {
+  // printf("%d,%d\n", start, end);
   if (start <= end) {
     for (int i = start, j = 0; i <= end; i++, j++) {
       if (source[i] != '\0') {
         dest[j] = source[i];
       }
     }
-    return 0;
   } else {
-    return 1;
+    dest[0] = '\0';
   }
 }
--- src/utility/string.h
+++ src/utility/string.h
@@ -1,5 +1,5 @@
 
 int strExtrSeparator(char *dest, char *source, const char *separetor);
 
-int strExtrInterval(char *dest, char *source, int start, int end);
+void strExtrInterval(char *dest, char *source, int start, int end);
 
