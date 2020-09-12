374388b13b8a8a9890cb0f8b6d767d512e23d5d9
Wabri <gabriele.puliti@protonmail.com>
2020-09-12 19:59:43 +0200

    Refactor pfc: extract run function for pfc

--- src/pfc/pfc.c
+++ src/pfc/pfc.c
@@ -1,58 +1,30 @@
-#include "../constants.h"
+#include "pfc.h"
 #include "structure.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <wait.h>
 
-int main(int argc, char *argv[]) {
+void run(char *name, char *filePath, char *logPath, char *sentence) {
+  // printf("pfc: %d\n", getpid());
+  PFC pfc, *ppfc = &pfc;
+  PTP ptp, *pptp = &ptp;
+  ppfc->name = name;
+  ppfc->filePath = filePath;
 
-  // creazione 3 processi
+  char *logPathName = malloc(1 + strlen(logPath) + strlen("log_") +
+                             strlen(ppfc->name) + strlen(".txt"));
+  logPathName[0] = '\0';
 
-  // PFC1
-  if (fork() == 0) {
-    //printf("pfc1: %d\n", getpid());
-    PFC pfc1, *ppfc1 = &pfc1;
-    PTP ptp1, *pptp1 = &ptp1;
-    ppfc1->name = "PFC1";
-    ppfc1->filePath = "resources/G18.txt";
-    printf("%s Inizializzato con successo \n", ppfc1->name);
-    importNMEA(ppfc1, pptp1, PFC_1_SENTENCE);
-    printPTPLength(pptp1);
-    exit(EXIT_SUCCESS);
-  }
+  strcat(logPathName, logPath);
+  strcat(logPathName, "log_");
+  strcat(logPathName, ppfc->name);
+  strcat(logPathName, ".txt");
 
-  // PFC2
-  if (fork() == 0) {
-    //printf("pfc2: %d\n", getpid());
-    PFC pfc2, *ppfc2 = &pfc2;
-    PTP ptp2, *pptp2 = &ptp2;
-    ppfc2->name = "PFC2";
-    ppfc2->filePath = "resources/G18.txt";
-    printf("%s Inizializzato con successo \n", ppfc2->name);
-    importNMEA(ppfc2, pptp2, PFC_2_SENTENCE);
-    printPTPLength(pptp2);
-    exit(EXIT_SUCCESS);
-  }
+  // printf("%s\n", filePath);
+  // printf("%s\n", logPathName);
 
-  // PFC3
-  if (fork() == 0) {
-    //printf("pfc3: %d\n", getpid());
-    PFC pfc3, *ppfc3 = &pfc3;
-    PTP ptp3, *pptp3 = &ptp3;
-    ppfc3->name = "PFC3";
-    ppfc3->filePath = "resources/G18.txt";
-    printf("%s Inizializzato con successo \n", ppfc3->name);
-    importNMEA(ppfc3, pptp3, PFC_3_SENTENCE);
-    printPTPLength(pptp3);
-    exit(EXIT_SUCCESS);
-  }
+  ppfc->fileLog = logPathName;
 
-  //
-  wait(NULL);
-  wait(NULL);
-  wait(NULL);
-
-  exit(EXIT_SUCCESS);
+  importNMEA(ppfc, pptp, sentence);
+  printPTPLength(pptp);
 }
---
+++ src/pfc/pfc.h
@@ -0,0 +1,2 @@
+
+void run(char *name, char *filePath, char *logPath, char *sentence);
