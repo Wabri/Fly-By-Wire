109d6130965590fff3322bf12dccaccabafe1790
Wabri <gabriele.puliti@protonmail.com>
2021-01-03 18:33:26 +0100

    Update main with a process counter

--- src/main.c
+++ src/main.c
@@ -14,46 +14,51 @@
 
 int main(int argc, char *argv[]) {
     char* g18Path = extractG18FromArgument(argc, argv);
+    int processCounter = 0;
 
     if (!isFileExistsAccess(g18Path)){
         exit(EXIT_FAILURE);
     }
 
     int *pidPFCs = malloc(sizeof(int[3]));
-    
+
+    processCounter += 1;
     pidPFCs[0] = fork();
     if (pidPFCs[0] == 0) {
         pfc(g18Path, PFC_SOCK_SENTENCE, PFC_TRANS_SOCKET);
         exit(EXIT_SUCCESS);
     }
 
+    processCounter += 1;
     pidPFCs[1] = fork();
     if (pidPFCs[1] == 0) {
         pfc(g18Path, PFC_PIPE_SENTENCE, PFC_TRANS_PIPE);
         exit(EXIT_SUCCESS);
     }
 
+    processCounter += 1;
     pidPFCs[2] = fork();
     if (pidPFCs[2] == 0) {
         pfc(g18Path, PFC_FILE_SENTENCE, PFC_TRANS_FILE);
         exit(EXIT_SUCCESS);
     }
 
+    processCounter += 1;
     if (fork() == 0) {
         transducer();
         exit(EXIT_SUCCESS);
     }
-    
+
+    processCounter += 1;
     if (fork() == 0) {
         fman(pidPFCs);
         exit(EXIT_SUCCESS);
     }
 
-    wait(NULL);
-    wait(NULL);
-    wait(NULL);
-    wait(NULL);
-    wait(NULL);
+    while (processCounter) {
+        wait(NULL);
+        processCounter -= 1;
+    }
 
     exit(EXIT_SUCCESS);
 }
